ee2211a282ee08b7fd780cbaa7ff3c9b07d1fa87
SinghDigamber <digambersingh126@gmail.com>
2019-08-23 02:22:27 +0530

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# React Radio Button Tutorial with Example
+[React Radio Button Tutorial with Example](https://www.positronx.io/react-radio-button-tutorial-with-example/)
 
 ## Available Scripts
 
@@ -66,3 +67,4 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+# react-radio-buttons
--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      color: 'green'
+    };
+
+    this.onRadioChange = this.onRadioChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+  }
+
+  onRadioChange = (e) => {
+    this.setState({
+      color: e.target.value
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.onSubmit}>
+          <strong>Select Color:</strong>
+
+          <ul>
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="red"
+                  checked={this.state.color === "red"}
+                  onChange={this.onRadioChange}
+                />
+                <span>Red</span>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="green"
+                  checked={this.state.color === "green"}
+                  onChange={this.onRadioChange}
+                />
+                <span>Green</span>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="blue"
+                  checked={this.state.color === "blue"}
+                  onChange={this.onRadioChange}
+                />
+                <span>Blue</span>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="orange"
+                  checked={this.state.color === "orange"}
+                  onChange={this.onRadioChange}
+                />
+                <span>Ornage</span>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="purple"
+                  checked={this.state.color === "purple"}
+                  onChange={this.onRadioChange}
+                />
+                <span>Purple</span>
+              </label>
+            </li>
+          </ul>
+
+          <button type="submit">Choose Color</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
